bcd7c9bcb0dd029a9d1a3a105dcf989b6bdf7977
davidfreitas-dev <davidfreitas.dev@outlook.com>
2022-01-13 10:19:18 -0300

    Formulário concluído

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="draw__form">
-      <textarea cols="30" rows="5" placeholder="Digite aqui os nomes que quer sortear" v-model="values" @blur="handleBlur"></textarea>
-      <input type="text" placeholder="Quantidade a ser sorteada por vez" v-model="qtdItems">
+      <textarea cols="30" rows="5" placeholder="Digite aqui os nomes que quer sortear" v-model="names" @blur="handleBlur"></textarea>
+      <input type="text" placeholder="Quantidade a ser sorteada por vez" v-model="qtdDraw">
   </div>
 </template>
 
@@ -9,20 +9,27 @@
 export default {
   data() {
     return {
-      values: '',
-      qtdItems: 1
+      names: '',
+      qtdDraw: 1
     }
   },
   methods: {
     handleBlur() {
-      const values = this.values.split(',')
-      console.log('Array de nome: ' + values + ', Qtd Items: ' + this.qtdItems)
+      const names = this.names.split(',')
+      this.$emit('countNames', names)
     }
   }
 }
 </script>
 
 <style>
+  .draw__form {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    grid-gap: 1rem;
+  }
+
   textarea, input {
     color: var(--font);
     font-size: 1rem;
--- src/components/Raffle.vue
+++ src/components/Raffle.vue
@@ -1,32 +1,27 @@
 <template>
-  <div class="raffle">
-      <textarea cols="30" rows="5" placeholder="Digite aqui os nomes que quer sortear"></textarea>
-      <input type="text" placeholder="Quantidade a ser sorteada por vez">
-      <button @click="draw">Sortear</button>
-      <ul>
-        <li v-for="(value, i) in values" :key="i">
-          <span>{{ i == random ? value : '' }}</span>
-        </li>
-      </ul>
+  <div class="raffle">      
+      <Form @countNames="countNames"/>
+      <Button />
+      <Display />      
   </div>
 </template>
 
 <script>
+import Form from './Form.vue'
+import Button from './Button.vue'
+import Display from './Display.vue'
 export default {
   props: ['values'],
+  components: { Form, Button, Display },
   data() {
     return {
-      random: null
+      qtdNames: 0
     }
   },
   methods: {
-    draw() {
-      const max = this.values.length
-      const random = Math.floor(Math.random() * max)
-      
-      this.random = random
-      
-      this.$emit('remove', random)
+    countNames(names) {
+      const count = names.length
+      this.qtdNames = count
     }
   }
 }
@@ -39,36 +34,5 @@ export default {
     align-items: center;
     grid-gap: 1rem;
     padding: 2rem;
-  }
-
-  textarea, input {
-    color: var(--font);
-    width: 350px;
-    padding: 1rem;
-    outline: none;
-    border-radius: 3px;
-    border: 1px solid var(--secondary);
-    background-color: var(--primary);
-  }
-
-  button {
-    color: #fcfcfc;
-    font-size: 1.1rem;
-    font-weight: 600;
-    padding: .5rem 1rem;
-    outline: none;
-    border: none;
-    border-radius: 3px;
-    background-color: var(--main);
-  }
-
-  ul {
-    list-style: none;
-    display: grid;
-    place-items: center;
-  }
-
-  span {
-    font-size: 1.3rem;
   }
 </style>
